88193989b4e623930bfdee7e56260b69631a36a7
amarbalu <inboxamarbalu@gmail.com>
2020-04-07 14:45:32 +0530

    host listener, content child

--- src/app/child-component/child-component.component.ts
+++ src/app/child-component/child-component.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit,HostBinding } from '@angular/core';
+import { Component, OnInit,HostBinding,
+HostListener } from '@angular/core';
+import {HelloComponent} from "../hello.component"
 
 @Component({
   selector: 'app-child-component',
@@ -6,20 +8,40 @@ import { Component, OnInit,HostBinding } from '@angular/core';
   styleUrls: ['./child-component.component.css']
 })
 export class ChildComponentComponent implements OnInit {
-
+user="amar"
   constructor(){
-    console.log("1.constructor")
+    console.log(" child - 1.constructor")
   }
+  //Host binding binds the property
     @HostBinding ('style.color') color="orange";
+  
+//Host binding binds the events to host event
+    @HostListener('click') onClick(){
+      alert("Hostlistener works")
+    }
+
    //ngOnChanges - Input param - changes captured or proprties change captured,
   //  change in fn param - conatins each input param in obect with properties like firstChange,previousValue,currentValue
   ngOnChanges(change):void{
-    console.log("Input param - changes captured ",change)
+    console.log("child - Input param - changes captured ",change)
+  }
+   ngAfterViewInit():void{
+console.log('child - viewInit after view init')
+  }
+    ngAfterViewChecked():void{
+    console.log('child - viewchecked after view checked')
   }
    //Init - Initialised once in a lifecycle of a component
   ngOnInit():void{
-    console.log("Initialised once in lifecycle of component")
+    console.log("child -Initialised once in lifecycle of component")
   }
+   ngAfterContentInit():void{
+    console.log("child-content init")
+  }
+  ngAfterContentChecked():void{
+      console.log("child-content checked")
+  }
+  
   ngOnDestroy():void{
 console.log("Destroy of component")
   }
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,9 +1,12 @@
-import { Component, Input,HostBinding } from '@angular/core';
+import { Component, Input,HostBinding,ViewChild,ContentChild } from '@angular/core';
+import {ChildComponentComponent} from "./child-component/child-component.component";
 
 @Component({
   selector: 'hello',
   template: `<h1 >Hello {{name}}!</h1>
-  <app-child-component></app-child-component>`,
+  <app-child-component>Hi I got value from hello - {{name}}</app-child-component>
+  <br/>
+ `,
   styles: [`h1 { font-family: Lato; }`],
   host:{
     'style.color': 'red',
@@ -14,20 +17,36 @@ import { Component, Input,HostBinding } from '@angular/core';
 export class HelloComponent  {
   @Input() name: string;
   constructor(){
-    console.log("1.constructor")
+    console.log("hello - 1.constructor")
   }
-  bgColor="100px"
+  @ViewChild(ChildComponentComponent,{static:true}) childComp:ChildComponentComponent ;
+    @ContentChild(ChildComponentComponent) contentComp:ChildComponentComponent ;
+  bgColor="100px" //--hostbinding in decorator
   //HostBinding helps to change it host tag property as variable 
   // it can be used further
   @HostBinding ('style.color') color="pink";
    //ngOnChanges - Input param - changes captured or proprties change captured,
   //  change in fn param - conatins each input param in obect with properties like firstChange,previousValue,currentValue
   ngOnChanges(change):void{
-    console.log("Input param - changes captured ",change)
+    console.log("hello -Input param - changes captured ",change)
+  }
+  ngAfterViewInit():void{
+console.log('hello - viewInit '+JSON.stringify(this.childComp))
+  }
+  //changes need to implement after child render can be implemented here get viewchild - we need to give a timeout as angular follows unidirectional way pf binding
+    ngAfterViewChecked():void{
+    console.log('hello - viewchecked'+JSON.stringify(this.childComp))
   }
    //Init - Initialised once in a lifecycle of a component
   ngOnInit():void{
     console.log("Initialised once in lifecycle of component")
+  }
+  ngAfterContentInit():void{
+    console.log("hello-content init"+JSON.stringify(this.contentComp))
+  }
+  ngAfterContentChecked():void{
+      console.log("hello-content checked"
+      +JSON.stringify(this.contentComp))
   }
   ngOnDestroy():void{
 console.log("Destroy of component")
